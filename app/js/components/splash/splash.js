ee0775d8f070084d9720fb771cb58681fb6819e3
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2017-01-09 16:40:41 +0000

    Splash page positioning

--- app/js/components/splash/splash.js
+++ app/js/components/splash/splash.js
@@ -4,28 +4,50 @@ import { TimelineMax } from 'gsap';
 export default class Splash {
 
     init(element) {
-        console.log(element);
 		this.panel = element;
 
-		this.smLarge = this.panel.find('#sm-large');
-		this.smSmall = this.panel.find('#sm-small');
-		this.radialLarge = this.panel.find('#radial-large');
-		this.radialLargeBlocks = this.panel.find('#radial-large-blocks');
+		this.dotsSmall = this.panel.find('.splash__dots__small');
+		this.dotsLarge = this.panel.find('.splash__dots__large');
+
+		this.cornerCircle = this.panel.find('.splash__corner-circle');
+
+		this.smRadialOuter = this.panel.find('.splash__sm-radial__outer');
+		this.smRadialInner = this.panel.find('.splash__sm-radial__inner');
+		this.smRadialBlocks = this.panel.find('.splash__sm-radial__blocks');
+		this.smRadialLines = this.panel.find('.splash__sm-radial__lines');
+
+		this.lrgRadialContainer = this.panel.find('.splash__lrg-radial__group');
+		this.lrgRadialLines = this.panel.find('.splash__lrg-radial__lines');
+		this.lrgRadialOuter = this.panel.find('.splash__lrg-radial__outer');
+		this.lrgRadialBlocks = this.panel.find('.splash__lrg-radial__blocks');
+		this.lrgRadialInner = this.panel.find('.splash__lrg-radial__inner');
+
 
 		this.startMainTl();
     }
 
 	setStage() {
-		const startTl = new TimelineMax();
+		const setStageTl = new TimelineMax();
 
-		startTl
-			.set(this.smLarge, { x: '-5%', scale: 0.75 })
-			.set(this.smSmall, { x: 115, y: 150, scale: 0.75 })
-			.set(this.radialLarge, { x: 645, y: 93, scale: 0.75 })
-			.set(this.radialLargeBlocks, { x: 645, y: 93, scale: 0.75 })
+		setStageTl
+			.set(this.dotsSmall, { x: '12%', scale: 0.80 })
+			.set(this.dotsLarge, { x: '8%', scale: 0.80 })
+
+			.set(this.cornerCircle, { x: '-40%', y: '80%', scale: 0.75 })
+
+			.set(this.smRadialOuter, { x: '-5%', scale: 0.75 })
+			.set(this.smRadialInner, { x: 115, y: 150, scale: 0.75 })
+			.set(this.smRadialBlocks, { x: 90, y: 124, scale: 0.75 })
+			.set(this.smRadialLines, { x: 72, y: 98, scale: 0.75 })
+
+			.set(this.lrgRadialLines, { x: 614, y: 66, scale: 0.75 })
+			.set(this.lrgRadialOuter, { x: 581, y: 36, scale: 0.75 })
+			.set(this.lrgRadialBlocks, { x: 645, y: 93, scale: 0.75 })
+			.set(this.lrgRadialInner, { x: 739, y: 190, scale: 0.75 })
+			.set(this.lrgRadialContainer, { x: -90, y: 40 })
 		;
 
-		return startTl;
+		return setStageTl;
 	}
 
 	startMainTl() {
