b8d026495e30bcbd143190d9cd85152d7ac32f4a
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2017-01-12 17:58:20 +0000

    gallery restructure

--- app/js/components/gallery/gallery.js
+++ app/js/components/gallery/gallery.js
@@ -6,14 +6,12 @@ export default class Gallery {
     init(element) {
 		this.panel = element;
 
-		this.svg = this.panel.find('.gallery__svg');
 		this.blocksTop = this.panel.find('.gallery__blocks--top');
 		this.blocksBottom = this.panel.find('.gallery__blocks--bottom');
 		this.linesTop = this.panel.find('.gallery__lines--top');
 		this.linesBottom = this.panel.find('.gallery__lines--bottom');
 		this.bkgnd = this.panel.find('.gallery__bkgnd');
 		this.frgnd = this.panel.find('.gallery__frgnd');
-		this.content = this.panel.find('.gallery__content div');
 
 		this.checkBreakpoint();
 
@@ -47,8 +45,6 @@ export default class Gallery {
 	setStageMobile() {
 		const setStageTl = new TimelineMax();
 
-		this.svg.attr('viewBox', '0 0 480 1024');
-
 		setStageTl
 			.set(this.blocksTop, { scale: 0.75, x: '2%', y: '32%' })
 			.set(this.blocksBottom, { scale: 0.75, x: '2%', y: '379%' })
@@ -68,16 +64,11 @@ export default class Gallery {
 	setStageDesktop() {
 		const setStageTl = new TimelineMax();
 
-		this.svg.attr('viewBox', '0 0 1280 1024');
-
 		setStageTl
-			.set(this.blocksTop, { scale: 0.75, x: '2%', y: '32%' })
-			.set(this.blocksBottom, { scale: 0.75, x: '2%', y: '379%' })
-			.set(this.linesTop, { scale: 0.75, y: '13%' })
-			.set(this.linesBottom, { scale: 0.75, x: '0', y: '397%' })
-			.set(this.bkgnd, { scaleX: 0.8, scaleY: 0.75, y: '10%' })
-			.set(this.frgnd, { scale: 0.8, scaleY: 0.75, y: '10%' })
-			.set(this.content, { x: '20%', y: '15%' })
+			.set(this.blocksTop, { scale: 0.75 }) // 2, 32
+			.set(this.blocksBottom, { scale: 0.75 }) // 2, 379
+			.set(this.linesTop, { scale: 0.75 }) // 13
+			.set(this.linesBottom, { scale: 0.75 }) // 0, 397
 		;
 
 		return setStageTl;
