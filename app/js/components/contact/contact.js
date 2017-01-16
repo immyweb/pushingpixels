20097513a22b8803abd8d0df87b0032acc2f4cbf
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2017-01-16 11:41:20 +0000

    Contact page more responsiveness

--- app/js/components/contact/contact.js
+++ app/js/components/contact/contact.js
@@ -5,10 +5,10 @@ export default class Contact {
     init(element) {
 		this.panel = element;
 
-		this.radialBlocks = this.panel.find('.contact__radial--blocks');
-		this.radialLines = this.panel.find('.contact__radial--lines');
-		this.dotsLarge = this.panel.find('.contact__dots--large');
-		this.dotsSmall = this.panel.find('.contact__dots--small');
+		this.radialBlocks = this.panel.find('.contact__svg__radials--blocks');
+		this.radialLines = this.panel.find('.contact__svg__radials--lines');
+		this.dotsLarge = this.panel.find('.contact__svg__dots--large');
+		this.dotsSmall = this.panel.find('.contact__svg__dots--small');
 
 		this.startMainTl();
     }
@@ -23,9 +23,6 @@ export default class Contact {
 	startMainTl() {
 		const mainTl = new TimelineMax();
 
-		mainTl
-			// .add(this.setStage())
-			// .add(this.radialsTl())
-		;
+
 	}
 }
