1ecdf7e70924adc87a8092fe6dcc4b6a75201abf
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2017-01-26 17:14:40 +0000

    safari get scale issue fix

--- app/js/utils/getScale.js
+++ app/js/utils/getScale.js
@@ -1,16 +1,28 @@
 import $ from 'jquery';
 
 export default function getScale(element) {
+	let scale;
 
-	let matrix = $(element).css('transform'),
-		values = matrix.split('(')[1];
+	let isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
 
-	values = values.split(')')[0];
-	values = values.split(',');
-	let a = values[0];
-	let b = values[1];
+	if ( !isSafari ) {
 
-	let scale = Math.sqrt(a*a + b*b);
+		let matrix = $(element).css('transform') || $(element).css('-ms-transform');
+
+		let	values = matrix.split('(')[1];
+
+		values = values.split(')')[0];
+		values = values.split(',');
+		let a = values[0];
+		let b = values[1];
+
+		scale = Math.sqrt(a*a + b*b);
+
+	}
+
+	// console.log(new WebKitCSSMatrix(window.getComputedStyle($(element)[0]).webkitTransform));
+	// console.log(matrix);
+
 
 	return scale;
 }
