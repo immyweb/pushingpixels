48373776bcc7a99ee3da57c8ca5af407315b8f0d
Imran Oozeerally <>
2018-08-09 13:12:16 +0100

    Added to new machine

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
 // DEPENDENCIES //
-const gulp              = require('gulp');
-const runSequence       = require('run-sequence');
-const requireDir        = require('require-dir');
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const requireDir = require('require-dir');
 
 requireDir('./gulp/tasks');
 
 // Create local development environment
-gulp.task('default', (callback) => {
-    runSequence(
-        'clean:dev',
-        ['lint:js', 'lint:scss'],
-        ['sass', 'bundleJS', 'nunjucks'],
-        ['browserSync', 'watch'],
-        callback
-    )
+gulp.task('default', callback => {
+	runSequence(
+		'clean:dev',
+		['lint:js', 'lint:scss'],
+		['sass', 'bundleJS', 'nunjucks'],
+		['browserSync', 'watch'],
+		callback
+	);
 });
 
 // Create/update svg iconfont set
 gulp.task('icons', ['iconfont', 'sass']);
 
 // Build prodution version for deployment
-gulp.task('build', (callback) => {
-    runSequence(
-        ['clean:dev', 'clean:dist'],
-        ['lint:js', 'lint:scss'],
-        ['sass', 'bundleJS', 'nunjucks'],
-        ['move:css', 'move:images', 'move:fonts', 'move:js', 'move:html'],
-        callback
-    )
+gulp.task('build', callback => {
+	runSequence(
+		['clean:dev', 'clean:dist'],
+		['lint:js', 'lint:scss'],
+		['sass', 'bundleJS', 'nunjucks'],
+		['move:css', 'move:images', 'move:fonts', 'move:js', 'move:html'],
+		callback
+	);
 });
